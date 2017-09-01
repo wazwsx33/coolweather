e6ab393de9c0a59a22059ac9977e6c225a566097
wazwsx33 <wazwsx33@outlook.com>
2017-09-01 14:37:31 +0800

    完成遍历省市县三级列表的功能

---
+++ app/src/main/java/com/android/coolweather/util/HttpUtil.java
@@ -0,0 +1,16 @@
+package com.android.coolweather.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by IdeaPad on 2017/8/29.
+ */
+
+public class HttpUtil {
+    public static void sendOkHttpRequest(String address, okhttp3.Callback callback){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
